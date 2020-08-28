85914415524a35448d06819ac4f574111458d854
antonio <antonagre@gmail.com>
2020-08-28 15:12:08 +0200

    28/08_02

--- src/GameMap.java
+++ src/GameMap.java
@@ -11,34 +11,42 @@ public class GameMap {
     private Container cp;
     private String[] OPS= {"+","-","/","*"};
     public int[][] map = {
-            {1,1,2},
-            {1,2,2},
-            {3,3,3}};
-    public int mapSide=3;
+            {1,1,2,4,4},
+            {1,2,2,4,6},
+            {3,3,5,6,6},
+            {3,5,5,6,7},
+            {3,5,6,6,7}};
+    public int mapSide=4;
     public HashMap<Integer,Block> blocks = new HashMap<Integer, Block>();
 
     private GameMap(int mapSide) {
         this.mapSide=mapSide;
     }
 
+    public static GameMap getInstance(){
+        if(ISTANCE==null){
+            ISTANCE = new GameMap(5);
+        }
+        return ISTANCE;
+    }
+
     public void createMap(){
         Random rand=new Random();
         for (int i=1;i<mapSide+1;i++){
-            String OP=OPS[rand.nextInt(4)];
-            Block b=new Block(i);
-            b.setOperation(OP);
-            blocks.put(i,b);
+
         }
         System.out.println(mapSide);
         for(int x=0;x<mapSide;x++){
             for(int y=0;y<mapSide;y++){
                 int id=map[x][y];
-                if( blocks.containsKey(id)){
-                    Cell c = blocks.get(id).addCell(x,y);
-                    for(int i=0;i<mapSide+1;i++){
-                        if(c.checkCellUpdate(i)){
-                            c.updateValueCell(i);
-                        }
+                String OP=OPS[rand.nextInt(4)];
+                Block b=new Block(id);
+                b.setOperation(OP);
+                blocks.put(id,b);
+                Cell c = blocks.get(id).addCell(x,y);
+                for(int i=0;i<mapSide+1;i++){
+                    if(c.checkCellUpdate(i)){
+                        c.updateValueCell(i);
                     }
                 }
             }
@@ -52,7 +60,7 @@ public class GameMap {
             frame.revalidate();
             frame.repaint();
             for (Cell c: b.cells) {
-                //c.reset();
+                c.reset();
             }
         }
         String j = Map.save(map,blocks);
@@ -65,7 +73,6 @@ public class GameMap {
     public boolean checkTop(int x,int y){
         if(y>1) {
             int id = map[x][y];
-            //System.out.println("CELL[" + x + "][" + y + "]=" + id + " TOP " + x + " " + (y - 1));
             if (map[x][y - 1] == id ) {
                 return true;
             }
@@ -76,7 +83,6 @@ public class GameMap {
     public boolean checkBottom(int x,int y){
         if(y < mapSide - 1) {
             int id = map[x][y];
-            //System.out.println("CELL[" + x + "][" + y + "]=" + id + " BOTTOM " + x + " " + (y + 1));
             if (map[x][y + 1] == map[x][y] ) {
                 return true;
             }
@@ -87,7 +93,6 @@ public class GameMap {
     public boolean checkLeft(int x,int y){
         if(x>1) {
             int id = map[x][y];
-            //System.out.println("CELL[" + x + "][" + y + "]=" + id + " LEFT " + (x - 1) + " " + y);
             if (map[x - 1][y] == map[x][y]) {
                 return true;
             }
@@ -98,7 +103,6 @@ public class GameMap {
     public boolean checkRight(int x,int y){
         if(x<mapSide-1) {
             int id = map[x][y];
-            //System.out.println("CELL[" + x + "][" + y + "]=" + id + " RIGHT " + (x + 1) + " " + y);
             if (map[x + 1][y] == map[x][y]) {
                 return true;
             }
@@ -113,21 +117,6 @@ public class GameMap {
 
     public JFrame getFrame(){
         return frame;
-    }
-
-    public static GameMap getInstance(int mapSide){
-        if(ISTANCE==null){
-            ISTANCE = new GameMap(mapSide);
-        }
-        return ISTANCE;
-    }
-
-
-    public static GameMap getInstance(){
-        if(ISTANCE==null){
-            ISTANCE = new GameMap(3);
-        }
-        return ISTANCE;
     }
 
 }
--- src/TestSwing.java
+++ src/TestSwing.java
@@ -6,63 +6,23 @@ import java.awt.event.ActionListener;
 
 public class TestSwing extends JFrame {
     GameMap map=GameMap.getInstance();
-    Integer[][] m=new Integer[3][3];
     public Container cp;
 
 
     public  TestSwing(int a){
         super("KENKEN-MAP");
         Container cp = getContentPane();
-        cp.setLayout(new GridLayout(3,3));
-        this.setResizable(false);
+        cp.setLayout(new GridLayout(map.mapSide,map.mapSide));
+        this.setResizable(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         map.setFrame(this);
         map.createMap();
         pack();
-        setSize(new Dimension(188, 216));
+        setSize(new Dimension(315, 345));
         setVisible(true);
         //Risolutore Res = new Risolutore(GameMap.getInstance().blocks.get(3));
     }
 
-    private class TestCell extends JButton{
-        int x,y;
-        String n;
-        Container cp;
-
-        public TestCell(int x,int y,int n){
-            this.x = x;
-            this.y = y;
-            cp=cp;
-            this.n = new Integer(n).toString();
-            Border emptyBorder = BorderFactory.createEmptyBorder();
-            setBorder(emptyBorder);
-            addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println("TestCell "+n+" pressed");
-                }
-            });
-        }
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            /*paint(g);
-            this.setBounds(this.x,this.y,60,60);
-            this.setLocation(this.x,this.y);
-            this.setSize(new Dimension(60,60));*/
-            Graphics2D g2 = (Graphics2D) g;
-            g2.setStroke(new BasicStroke(2));
-            g2.drawString(this.n,30,30);
-            g2.drawString("+",5,10);
-            g2.drawLine(0,0,60,0);
-            g2.setStroke(new BasicStroke(2));
-            g2.drawLine(60,0,60,60);
-            g2.setStroke(new BasicStroke(2));
-            g2.drawLine(60,60,0,60);
-            g2.setStroke(new BasicStroke(2));
-            g2.drawLine(0,60,0,0);
-        }
-    }
 
     public static void main(String[] args) {
         new TestSwing(1);
