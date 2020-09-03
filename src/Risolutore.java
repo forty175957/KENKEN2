209a5a9c07a21e5ebc61eed45762f9367df6c642
Lucky <fortunato96.fc@gmail.com>
2020-09-03 19:29:49 +0200

    MainMenu_00

--- src/Risolutore.java
+++ src/Risolutore.java
@@ -7,36 +7,26 @@ public class Risolutore {
 
         public void solve(Block b, ArrayList<Integer> excluded) {
             Cell c = b.findEmpty();
+            int index=0;
             if (c != null) {
                 while (!block.checkBlock()) {
                     for (int i = 1; i < GameMap.getInstance().mapSide + 1; i++) {
                         if (c.checkCellUpdate(i) && !excluded.contains(i)) {
                             c.updateValueCell(i);
                             c = b.findEmpty();
+                            if (c == null || index < block.cells.size() && !(block.checkBlock())) {
+                                c=b.cells.get(index);
+                                excluded.add(c.n);
+                                index+=1;
+                            }
                         }
                         if (i == GameMap.getInstance().mapSide) {
                             backtrack(b, excluded);
                         }
                     }
-                    c = b.findEmpty();
-                }
-            } else {
-
-                    while (!block.checkBlock()) {
-                        for (Cell ce : b.cells) {
-                            for (int i = 1; i < GameMap.getInstance().mapSide + 1; i++) {
-                                if (ce.checkCellUpdate(i) && !excluded.contains(i)) {
-                                ce.updateValueCell(i);
-                            }
-                                if (i == GameMap.getInstance().mapSide) {
-                                excluded.add(ce.n);
-                                backtrack(b, excluded);
-                            }
-                        }
-                    }
-
                 }
             }
+
         }
 
         private void backtrack(Block b, ArrayList<Integer> excluded) {
@@ -46,7 +36,7 @@ public class Risolutore {
                 System.out.println("Soluzione: " + sol.toString());
                 Cell last = sol.remove(r - 1);
                 excluded.add(last.n);
-                last.updateValueCell(0);
+                last.reset();
             }
             solve(b, excluded);
         }
