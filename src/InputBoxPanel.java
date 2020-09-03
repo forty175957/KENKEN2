fefdfab6bd5d444863adb31bb59d83a40342d3d0
antonio <antonagre@gmail.com>
2020-09-03 13:28:00 +0200

    03/09-2

--- src/InputBoxPanel.java
+++ src/InputBoxPanel.java
@@ -5,52 +5,24 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class InputBoxPanel extends JFrame{
-
-    public InputBoxPanel(Cell c){
+public class InputBoxPanel extends JFrame implements KeyListener{
+    JLabel label;
+    JButton setBt;
+    JButton resolveBt;
+    JTextField textfield1;
+    Cell c;
+    public InputBoxPanel(Cell cell){
         super("Edit Cell");
+        c=cell;
         setLocation(500,500);
         getContentPane().setLayout(new FlowLayout());
-        JTextField textfield1 = new JTextField("",10);
+        textfield1 = new JTextField("",10);
         JLabel label=new JLabel("INSERT VALUE");
         JButton setBt= new JButton("SET");
         JButton resolveBt= new JButton("RESOLVE");
-        addKeyListener(new KeyListener() {
-            @Override
-            public void keyReleased(KeyEvent keyEvent) {
+        textfield1.addKeyListener(this);
 
-            }
 
-            @Override
-            public void keyPressed(KeyEvent keyEvent) {
-                if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
-                    int val = new Integer(textfield1.getText());
-                    if (!c.checkCellUpdate(val)) {
-                        label.setText("INVALID VALUE");
-                    } else {
-                        dispose();
-                        c.updateValueCell(val);
-                        GameMap.getInstance().getFrame().getContentPane().revalidate();
-                        GameMap.getInstance().getFrame().repaint();
-                    }
-                }
-            }
-
-            @Override
-            public void keyTyped(KeyEvent keyEvent) {
-                if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
-                    int val = new Integer(textfield1.getText());
-                    if (!c.checkCellUpdate(val)) {
-                        label.setText("INVALID VALUE");
-                    } else {
-                        dispose();
-                        c.updateValueCell(val);
-                        GameMap.getInstance().getFrame().getContentPane().revalidate();
-                        GameMap.getInstance().getFrame().repaint();
-                    }
-                }
-            }
-        });
         setBt.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
@@ -78,6 +50,40 @@ public class InputBoxPanel extends JFrame{
         pack();
         setVisible(true);
 
+    }
+
+    public void keyReleased(KeyEvent keyEvent) {
+
+    }
+
+    public void keyPressed(KeyEvent keyEvent) {
+        System.out.println("KEYTYPED -> KeyChar : " + keyEvent.getKeyChar() +
+                "  KeyCode : " + keyEvent.getKeyCode());
+        if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
+            int val = new Integer(textfield1.getText());
+            if (!c.checkCellUpdate(val)) {
+                label.setText("INVALID VALUE");
+            } else {
+                dispose();
+                c.updateValueCell(val);
+                GameMap.getInstance().getFrame().getContentPane().revalidate();
+                GameMap.getInstance().getFrame().repaint();
+            }
+        }
+    }
+
+    public void keyTyped(KeyEvent keyEvent) {
+        if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
+            int val = new Integer(textfield1.getText());
+            if (!c.checkCellUpdate(val)) {
+                label.setText("INVALID VALUE");
+            } else {
+                dispose();
+                c.updateValueCell(val);
+                GameMap.getInstance().getFrame().getContentPane().revalidate();
+                GameMap.getInstance().getFrame().repaint();
+            }
+        }
     }
 
 }
